26e80984e09012aeba6767027dfd594d8c4d33a7
Andy Holt <andy.holt@kmp.co.uk>
2016-12-01 16:47:45 +0000

    Continued work on visibility determination.

--- js/editor/services/sector-set-builder.js
+++ js/editor/services/sector-set-builder.js
@@ -6,15 +6,35 @@
 
             var self = this;
 
+            engine.staticMeshMathHelper.buildStaticMeshChunkCollisionFaces(staticMesh);
+            engine.staticMeshMathHelper.findStaticMeshPointCompletelyOutsideOfExtremities(staticMesh);
+
             var sectorSet = {
                 metrics: {
-                    sectorCount: [3, 1, 3],
-                    sectorSize: [10, 10, 10]
+                    sectorCount: [5, 2, 5],
+                    sectorSize: [4, 4, 4],
+                    rootOrigin: [-10, 0, 10]
                 },
                 sectors: []
             }
 
-            engine.staticMeshMathHelper.buildStaticMeshChunkCollisionFaces(staticMesh);
+            var sectorPointsByIndex = [];
+
+            self.forEachSector(sectorSet, function (sectorIndex, sectorOrigin) {
+
+                sectorPointsByIndex[sectorIndex] = [];
+
+                for (var i = 0; i < 1000; i++) {
+
+                    var point = vec3.create();
+                    
+                    self.createRandomPointWithinSector(point, sectorSet.metrics, sectorOrigin);
+
+                    if (engine.staticMeshMathHelper.determineIfPointIsWithinStaticMesh(point, staticMesh)) {
+                        sectorPointsByIndex[sectorIndex].push(point);
+                    }
+                }
+            });
 
             var line = new CollisionLine();
 
@@ -32,7 +52,22 @@
                         return;
                     }
 
-                    for (var i = 0; i < 10000; i++) {
+                    for (var i = 0; i < sectorPointsByIndex[sectorAIndex].length; i++) {
+                        for (var j = 0; j < sectorPointsByIndex[sectorBIndex].length; j++) {
+
+                            line.from = sectorPointsByIndex[sectorAIndex][i];
+                            line.to = sectorPointsByIndex[sectorBIndex][j];
+                            math3D.buildCollisionLineFromFromAndToPoints(line);
+
+                            if (self.determineIfLineIntersectsAnyFace(line, staticMesh)) {
+                                return;
+                            }
+
+                            sector.visibleSectorIndexes.push(sectorBIndex);
+                        }
+                    }
+
+                    /*for (var i = 0; i < 10000; i++) {
 
                         self.createRandomLineBetweenSectors(
                             line, sectorSet.metrics, sectorAOrigin, sectorBOrigin);
@@ -50,7 +85,7 @@
                         }
 
                         sector.visibleSectorIndexes.push(sectorBIndex);
-                    }
+                    }*/
                 });
             });
         },
@@ -60,17 +95,19 @@
             var index = 0;
             var origin = vec3.create();
 
-            for (var x = 0; x < engine.sectorSet.metrics.sectorCount[0]; x++) {
+            for (var x = 0; x < sectorSet.metrics.sectorCount[0]; x++) {
 
-                for (var y = 0; y < engine.sectorSet.metrics.sectorCount[1]; y++) {
+                for (var y = 0; y < sectorSet.metrics.sectorCount[1]; y++) {
 
-                    for (var z = 0; z < engine.sectorSet.metrics.sectorCount[2]; z++) {
+                    for (var z = 0; z < sectorSet.metrics.sectorCount[2]; z++) {
 
                         vec3.set(
                             origin,
                             x * sectorSet.metrics.sectorSize[0],
                             y * sectorSet.metrics.sectorSize[1],
                             z * -sectorSet.metrics.sectorSize[2]);
+
+                        vec3.add(origin, sectorSet.metrics.rootOrigin, origin);
 
                         callback(index, origin);
 
@@ -80,13 +117,13 @@
             }
         },
 
-        createRandomLineBetweenSectors: function (out, sectorMetrics, sectorAOrigin, sectorBOrigin) {
+        /*createRandomLineBetweenSectors: function (out, sectorMetrics, sectorAOrigin, sectorBOrigin) {
 
             this.createRandomPointWithinSector(out.from, sectorMetrics, sectorAOrigin);
             this.createRandomPointWithinSector(out.to, sectorMetrics, sectorBOrigin);
 
             math3D.buildCollisionLineFromFromAndToPoints(out);
-        },
+        },*/
 
         createRandomPointWithinSector: function (out, sectorMetrics, sectorOrigin) {
 
@@ -109,7 +146,7 @@
 
                     var collisionFace = chunk.collisionFaces[faceIndex];
 
-                    var faceIntersectionType = math3D.calculateCollisionLineIntersectionWithCollisionFace(faceIntersection, collisionLine, collisionFace)
+                    var faceIntersectionType = math3D.calculateCollisionLineIntersectionWithCollisionFace(null, line, collisionFace)
 
                     if (faceIntersectionType != FaceIntersectionType.None) {
 
