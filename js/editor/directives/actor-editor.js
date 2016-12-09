18d7592ad3b5571f9f8232dec1df284c4837c87b
Andy Holt <andy.holt@kmp.co.uk>
2016-12-09 12:36:01 +0000

    Editor improvements.

--- js/editor/directives/actor-editor.js
+++ js/editor/directives/actor-editor.js
@@ -1,4 +1,4 @@
-﻿editorApp.directive('actorEditor', [function () {
+﻿editorApp.directive('actorEditor', ['$rootScope', function ($rootScope) {
     return {
         templateUrl: 'html/editor/directives/actor-editor.html',
         scope: {
@@ -6,6 +6,50 @@
         },
         controller: function ($scope) {
 
+            $scope.chooseStaticMesh = function () {
+
+                $rootScope.$broadcast('choose-resource', {
+                    resourceType: 'static-mesh',
+                    callback: function (resourceId) {
+                        $scope.actor.staticMeshId = resourceId;
+                    }
+                });
+            }
+
+            $scope.clearStaticMesh = function () {
+
+                $scope.actor.staticMeshId = null;
+            }
+
+            $scope.chooseSkinnedMesh = function () {
+
+                $rootScope.$broadcast('choose-resource', {
+                    resourceType: 'skinned-mesh',
+                    callback: function (resourceId) {
+                        $scope.actor.skinnedMeshId = resourceId;
+                    }
+                });
+            }
+
+            $scope.clearSkinnedMesh = function () {
+
+                $scope.actor.skinnedMeshId = null;
+            }
+
+            $scope.chooseSkinnedMeshAnimation = function () {
+
+                $rootScope.$broadcast('choose-resource', {
+                    resourceType: 'skinned-mesh-animation',
+                    callback: function (resourceId) {
+                        $scope.actor.skinnedMeshAnimationId = resourceId;
+                    }
+                });
+            }
+
+            $scope.clearSkinnedMeshAnimation = function () {
+
+                $scope.actor.skinnedMeshAnimationId = null;
+            }
         },
         link: function ($scope, element, attrs) {
 
